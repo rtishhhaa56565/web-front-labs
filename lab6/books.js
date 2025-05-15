8e97f900f492191363da2bb5653bb0b80c856660
Арина <arina.arysheva2005@mail.ru>
2025-05-15 19:24:30 +0700

    Книги

---
+++ lab6/books.js
@@ -0,0 +1,92 @@
+const books = [
+    {
+        title: "Преступление и наказание",
+        author: "Фёдор Достоевский",
+        price: 450,
+        year: 1866,
+        genre: "Классика",
+        bestseller: true,
+        inStock: true
+    },
+    {
+        title: "1984",
+        author: "Джордж Оруэлл",
+        price: 380,
+        year: 1949,
+        genre: "Антиутопия",
+        bestseller: true,
+        inStock: true
+    },
+    {
+        title: "Мастер и Маргарита",
+        author: "Михаил Булгаков",
+        price: 520,
+        year: 1967,
+        genre: "Классика",
+        bestseller: true,
+        inStock: false
+    },
+    {
+        title: "Три товарища",
+        author: "Эрих Мария Ремарк",
+        price: 490,
+        year: 1936,
+        genre: "Роман",
+        bestseller: false,
+        inStock: true
+    },
+    {
+        title: "Маленький принц",
+        author: "Антуан де Сент-Экзюпери",
+        price: 350,
+        year: 1943,
+        genre: "Философская сказка",
+        bestseller: true,
+        inStock: true
+    },
+    {
+        title: "Атлант расправил плечи",
+        author: "Айн Рэнд",
+        price: 890,
+        year: 1957,
+        genre: "Философский роман",
+        bestseller: false,
+        inStock: true
+    },
+    {
+        title: "Шантарам",
+        author: "Грегори Дэвид Робертс",
+        price: 670,
+        year: 2003,
+        genre: "Приключения",
+        bestseller: true,
+        inStock: false
+    },
+    {
+        title: "Тень горы",
+        author: "Грегори Дэвид Робертс",
+        price: 720,
+        year: 2015,
+        genre: "Приключения",
+        bestseller: false,
+        inStock: true
+    },
+    {
+        title: "Игра Эндера",
+        author: "Орсон Скотт Кард",
+        price: 540,
+        year: 1985,
+        genre: "Фантастика",
+        bestseller: true,
+        inStock: true
+    },
+    {
+        title: "Песнь льда и пламени",
+        author: "Джордж Р. Р. Мартин",
+        price: 950,
+        year: 1996,
+        genre: "Фэнтези",
+        bestseller: true,
+        inStock: true
+    }
+];
